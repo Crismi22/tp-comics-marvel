3e17e7f2fbf4360fc253bcabaf19e6a5bf8ce1c8
Cristina <97647142+Crismi22@users.noreply.github.com>
2022-11-04 19:03:28 -0300

    update/se muestran comics/estilado de cards

--- js/api.js
+++ js/api.js
@@ -3,14 +3,13 @@
 const apiPublic = '913230280f095cfc021af9589db27af8';
 const apiPrivate = '374f83ad0294aad353f76515c7a64f3a33d3a292';
 const baseUrl = 'https://gateway.marvel.com/v1/public';
-;
+
+
+
 
 const getComics = async () => {
-
-  const response = await fetch(`${baseUrl}/comics?apikey${apiPublic}&offset=0&orderBy=title`);
+  const response = await fetch(`${baseUrl}/comics?apikey=${apiPublic}&offset=0&orderBy=title`);
+  const data = response.json();
+  return data
   
-  const data = await response.json();
-  return data;
-
-};
-
+}
--- js/app.js
+++ js/app.js
@@ -1,155 +1,51 @@
-// const searchType = document.querySelector('#search-type');
-// const results = document.querySelector('#results');
-// const resultsNumber = document.querySelector('.results-number');
-// const comicCover = document.querySelector('.comic-cover');
-// const comicTitle = document.querySelector('.comic-title');
-// const comicPublished = document.querySelector('.comic-published');
-// const comicWriters = document.querySelector('.comic-writers');
-// const comicDescription = document.querySelector('.comic-description');
-// const comicSection = document.querySelector('.comic-section');
-// const characterSection = document.querySelector('.character-section');
-// const resultSection = document.querySelector('.results-section');
-// const characterPortrait = document.querySelector('.character-portrait');
-// const characterName = document.querySelector('.character-name');
-// const characterDescription = document.querySelector('.character-description');
-// const searchButton = document.querySelector('.search-button');
-// const loaderContainer = document.querySelector('.loader-container');
-// const searchInput = document.querySelector('.search-input');
 
-// //********loader
-// const loader = document.querySelector('.loader-container')
+const loadComics = async () => {
+    const comicsResponse = await getComics();
+    const data = comicsResponse.data //nos trae de la paginacion "offset"
+    const comics = data.results;
 
-// //****paginador
-// const paginaActual = document.getElementById('pagina-actual');
-// const totalPaginas = document.getElementById('total-paginas');
-// const firstPage = document.querySelector('.first-page');
-// const previusPage = document.querySelector('.previus-page');
-// const nextPage = document.querySelector('.next-page');
-// const lastPage = document.querySelector('.last-page');
+    console.log(data)
 
-// //  ******************** 
-// //  API MARVEL INICIAL
-// //  *******************
+    const results = document.getElementById('comics-results');
+    const container = document.createElement('div');
+    const row = document.createElement('div');
 
-// const apiPublic = '913230280f095cfc021af9589db27af8';
-// const apiPrivate = '374f83ad0294aad353f76515c7a64f3a33d3a292';
-// const baseUrl = 'https://gateway.marvel.com/v1/public';
-// // const url = `http://gateway.marvel.com/v1/public/comics/?apikey=${apiPublic}&offset=${offset}`;
-
-// // *******PAGINADOR********
-// let offset = 0;
-// let resultsCount = 0;
-
-
-// const getSearchParams = (isSearch) => {
-//     let searchParams = `?apikey=${apiPublic}&offset=${offset}`
-  
-//     if(!isSearch){
-//       return searchParams
-//     }
-  
-//     // console.log(isSearch)
-
-//   if (searchType.value === 'comics') {
-//     searchParams += `${searchType.value}${searchParams}`;
-//   }
-
-//   return searchParams
-// };
-
-// // getSearchParams()
-// const getApiUrl = (resourse, resourseId, subResourse) => {
-//     const isSearch = !resourseId && !subResourse;
-//     let url = `${baseUrl}${resourse}`
-//     // console.log(url)
-
-//     if(resourseId){
-//         url += `/${resourseId}`
-//     }
+    results.appendChild(container)
+    container.appendChild(row)
     
-//     if(subResourse){
-//         url += `/${subResourse}`
-//     }
+    container.classList.add('container');
+    row.classList.add('row');
     
-//     url += getSearchParams(isSearch) // ?apikey=${apiPublic}&offset=${offset}
-//     return url; 
-// }
-// const updateresultsCount = count => {
-//     resultsNumber.innerHTML = count;
-//     resultsCount = count;
-// } 
-
-// const fecthUrl = async url => {
-//     const response = await fetch(url)
-//     const json = await response.json()
-//     return json 
-// }
-
-// const fetchComics = async() => {
-//     const {data : {results, total}
-//         } = await fecthUrl(getApiUrl('comics'))
-//     console.log(results)
-//     console.log(total)
-//     printComics(results)
-
-// }
-
-// // **************************
-// //         PINTAR COMICS
-// // **************************
-
-// const printComics = comics => {
-//     if(comics.length === 0){
-//         results.innerHTML = '<h2 class="not-results">No hemos encontrado resultados</h2>'
-//     }
-//     for (const comic of comics) {
-//         const comicCard = document.createElement('div');
-//         comicCard.tabIndex = 0;
-//         comicCard.classList.add('comic');
-//         comicCard.onclick = () => {
-//             console.log(comic, comic.id)
-//         }
-//         comicCard.innerHTML = `
-//         <div class="comic-img-container">
-//           <img src="${comic.thumbnail.path}/portrait_xlarge.${comic.thumbnail.extension}" 
-//             alt="${comic.title}" class="comic-thumbnail" />
-//           </div>
-//           <h3 class="comic-title">${comic.title}</h3>`
-          
-//         results.append(comicCard)
-//     } 
-// }
-// const loadDetail = (comic) => {
-
-//     const comicDetail = document.getElementById('comic-detail');
-//     const title = document.createElement('h3');
-//     const text = document.createElement(comic.title);
-//     const div = document.createElement('div');
-
-//     title.appendChild(text);
-
-//     comicDetail.appendChild(title);
-//     comicDetail.appendChild(comic.title)
-// }
-
-// const search = () => {
-//     if(searchType.value === 'comics')
-//     fetchComics()
-// }
 
 
-// const inicio = () => {
-//    search()
-// }
+    comics.forEach(comic => {
+        console.log(comic)
+
+        const card = document.createElement('div');
+        const cardImg = document.createElement('img');
+        const cardBody = document.createElement('div');
+        const colum = document.createElement('div');
+        const title = document.createElement('h3')
+
+        const texTitle = document.createTextNode(comic.title)
+
+        card.appendChild(cardImg);
+        card.appendChild(cardBody);
+        colum.appendChild(card);
+        cardBody.appendChild(title);
+        title.appendChild(texTitle);
+
+        card.classList.add('card');
+        cardImg.classList.add('card-img-top');
+        cardBody.classList.add('card-body');
+        colum.classList.add('col-md-3');
+        title.classList.add('comic-title');
+
+        row.appendChild(colum)
+        cardImg.setAttribute('src', `${comic.thumbnail.path}.${comic.thumbnail.extension}`);
 
 
-// window.onload = inicio
+    });
+}
 
-//======================================================================================//
-
-
-
-//para el input
-// const text = e.target.searching.value;
-
-// console.log(text)
+loadComics();
